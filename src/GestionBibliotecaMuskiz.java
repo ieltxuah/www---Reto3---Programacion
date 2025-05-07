bfae38281c76e7f5409780e1e050350af79e5427
Erlaitz <erlaitz.alonso05@somo.eus>
2025-05-07 12:46:33 +0200

    Update GestionBibliotecaMuskiz.java

--- src/GestionBibliotecaMuskiz.java
+++ src/GestionBibliotecaMuskiz.java
@@ -185,7 +185,6 @@ public class GestionBibliotecaMuskiz {
 
                 case "2":
                     System.out.println("Has elegido: Bajas.");
-
                     // Aquí iría la lógica para bajas
                     System.out.print("Introduce el código del autor a eliminar: ");
                     String codAutorBaja = scanner.nextLine().trim();
